feab66f61f05883c89d96f4b27435bac1621b599
colbyjacobjensen <colbyjacobjensen@gmail.com>
2023-04-11 14:43:41 -0600

    RDS Changes

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ using INTEX2.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetConnectionString("BuffaloDBConnection");
 
 builder.Services.AddDbContext<BuffaloDbContext>(options =>
     options.UseNpgsql(connectionString));
