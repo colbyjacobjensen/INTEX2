e00136163f86eeb6530623ceda338a41615a04b7
christianhansen19 <chris.hansen1298@gmail.com>
2023-04-13 22:22:23 -0600

    Supervised is Updated

--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using INTEX2.Models;
-using Microsoft.ML.OnnxRuntime;
 
 var builder = WebApplication.CreateBuilder(args);
 
